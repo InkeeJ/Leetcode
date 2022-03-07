607d24fda2b4de010db583fca5088cd16700d968
InkeeJ <inkeejung@yonsei.ac.kr>
2022-03-07 11:27:26 +0900

    Lowered the L

--- 1.Two Sum.py
+++ 1.Two Sum.py
@@ -1,5 +1,5 @@
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         n=len(nums)
         memory={}
         for i in range(n):
