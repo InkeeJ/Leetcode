87e1dc1fe349e8853ef71ae8e0a3a7080551952e
InkeeJ <inkeejung@yonsei.ac.kr>
2022-03-24 16:55:27 +0900

    leetcode soln. I've tried several times with no faster than 1400ms. The last two algorithms are having less than 50 ms runtime.

---
+++ 69. sqrt(x).py
@@ -0,0 +1,65 @@
+'''
+very slow
+'''
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        n=0
+        while True:
+            if n*n <= x < (n+1)*(n+1) : break
+            else : n +=1
+        return n
+
+'''
+faster
+'''
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        n=0
+        while True:
+            if x - (n+1)*(n+1)<0 : break
+            else : n +=1
+        return n
+
+'''
+faster
+
+'''
+
+'''
+binary search method. much faster
+'''
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        n=0
+        if x == 0 or x == 1 : return x
+        while True:
+            if x - 2**(n+1)<0 : break # 2^n <= x < 2^(n+1)
+            else : n +=1
+        k = n//2
+        m = 2**k
+        while True:
+            if x - (m+1)*(m+1) <0 : break
+            else : m +=1
+        return m
+
+'''
+similar
+'''
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x == 0:
+            return 0
+        elif x == 1:
+            return 1
+        root = x / 2
+        eps = 0.00001
+        while(root - x / root > eps):
+            root = 0.5 * (root + x / root)
+        return int(root)
+
+
+'''
+Analyze the last two soln ! 
+'''
