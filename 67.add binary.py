cfbb46347d0e2532bc1cbeedbab6e533ca73f95e
InkeeJ <inkeejung@yonsei.ac.kr>
2022-03-29 17:19:15 +0900

    leetcode soln, `22 03 29

--- 67.add binary.py
+++ 67.add binary.py
@@ -1,30 +1,27 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        n = len(a)
-        m = len(b)
-        k = min(n,m)
-        temp = '0'
-        t=-1
-        c= ''
-        i=1
-        if n == 0 : return b
-        if m == 0 : return a
-        while i<=k:
-            if a[t] == 1 and a[t] == b[t]:
-                c = temp + c
-                temp = '1'
-                
-            elif a[t] == 0 and a[t] == b[t]:
-                c = temp + c
-                temp = '0'
-            else:
-                if temp == '1':
-                    c = '0' + c
-                    temp = '0'
-                else :
-                    c = '1' + c
-            i += 1
-            t -= 1
-        
+        a=a[::-1]
+        b=b[::-1]
+        num = 0
+        for i in range(len(a)):
+            if a[i] == '1':
+                num += 2**i
+            number_a = num
+        num = 0
+        for i in range(len(b)):
+            if b[i] == '1':
+                num += 2**i
+            number_b = num
+        number_c = number_a + number_b
+        if number_c == 0 or number_c == 1 : return str(number_c)
+        if number_c % 2 == 1 : number_c -= 1; answer = '1'
+        else : answer = '0'    
+        x= 2
+        while x <= number_c:
+            x = x*2
+            alpha = (number_c%x) // (x//2) 
+            answer = str(alpha) + answer
+            number_c = number_c - alpha * (x//2)
 
-            
+        return( answer)
+                
